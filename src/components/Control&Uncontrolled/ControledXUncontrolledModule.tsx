dab077740895bcbc4f33b6e557d2fdb7985ce439
luisfop <luisfop.dev@gmail.com>
2022-11-16 11:11:17 -0300

    feat: uncontrolled form

---
+++ src/components/Control&Uncontrolled/ControledXUncontrolledModule.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UncontrolledForm from './Uncontrolled/UncontrolledForm'
+
+const ControledXUncontrolledModule = () => {
+    return (
+        <div>
+            <UncontrolledForm />
+        </div>
+    )
+}
+
+export default ControledXUncontrolledModule
