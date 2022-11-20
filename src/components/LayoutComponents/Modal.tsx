25ccf32804e3c795c24ecddfeb76e13b701a4d63
luisfop <luisfop.dev@gmail.com>
2022-11-20 19:03:29 -0300

    feat:added modal to layout module

---
+++ src/components/LayoutComponents/Modal.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+
+const ModalBackGround = styled.div`
+position: fixed;
+z-index: 1;
+left: 0;
+top:0;
+width: 100%;
+height: 100%;
+overflow: auto;
+background-color: rgba(0,0,0,0.5);
+`;
+
+const ModalBody = styled.div`
+background-color: white;
+margin: 10% auto;
+padding: 20px;
+width: 50%auto;
+`;
+
+const Modal = ({ children }: { children: JSX.Element }) => {
+
+    const [shouldShow, setShouldShow] = useState(false);
+
+
+    return (
+        <>
+            <button onClick={() => setShouldShow(!shouldShow)}>Show modal</button>
+            {shouldShow && (
+                <ModalBackGround onClick={() => setShouldShow(false)}>
+                    <ModalBody onClick={(e) => e.stopPropagation()}>
+                        <button onClick={() => setShouldShow(false)}>hide modal</button>
+                        {children}
+                    </ModalBody>
+                </ModalBackGround>)}
+        </>
+    )
+}
+
+export default Modal;
